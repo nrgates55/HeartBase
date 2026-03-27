25a342eb1637d4dbf9d376daf872a5a2f871e161
nrgates55 <ohpanther05@gmail.com>
2026-03-27 16:28:15 -0500

    Add files via upload

---
+++ HeartBase/src/main/java/heart/backend/heartbase/HeartBaseApplication.java
@@ -0,0 +1,13 @@
+package heart.backend.heartbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartBaseApplication.class, args);
+    }
+
+}
